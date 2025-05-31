eab5ad54e9e47bdf849f2148f0938ee1a0499bd3
sunfkny <30853461+sunfkny@users.noreply.github.com>
2025-06-01 00:47:29 +0800

    New ui

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,11 +1,26 @@
 <template>
   <div>
-    <div class="games-nav-bar flex justify-center items-center gap-2">
+    <div
+      class="flex justify-center items-center gap-2 p-4"
+      :class="{ 'flex-col': vertical }"
+    >
+      <NuxtLink v-if="home" to="/">
+        <UButton
+          variant="outline"
+          color="neutral"
+          class="p-0"
+          :class="{ [roundedClass]: true }"
+        >
+          <div class="size-[64px] flex justify-center items-center">
+            <UIcon name="i-lucide-house" class="size-[32px]" />
+          </div>
+        </UButton>
+      </NuxtLink>
       <NuxtLink
         v-for="game in games"
         :key="game.key"
-        class="games-nav-bar-item rounded-2xl border-2 border-solid border-transparent p-1 transition-all duration-300 hover:border-gray-300"
-        :style="{ 'view-transition-name': `games-nav-bar-item-${game.key}` }"
+        class="border-2 border-solid border-transparent p-1 transition-all duration-300 hover:border-gray-300"
+        :class="{ [roundedClass]: true }"
         :to="`/announcement/${game.key}`"
       >
         <img
@@ -14,6 +29,8 @@
           :title="game.name"
           :width="props.size"
           :height="props.size"
+          class="games-icon"
+          :style="{ 'view-transition-name': `games-icon-${game.key}` }"
         />
       </NuxtLink>
     </div>
@@ -23,11 +40,27 @@
 <script setup lang="ts">
 import { games } from "~/constants/game";
 
-const props = defineProps({
-  size: {
-    type: Number,
-    default: 128,
-  },
+const props = withDefaults(
+  defineProps<{
+    size?: number;
+    vertical?: boolean;
+    home?: boolean;
+  }>(),
+  {
+    size: 128,
+    vertical: false,
+    home: false,
+  }
+);
+
+const roundedClass = computed(() => {
+  if (props.size <= 80) {
+    return "rounded-xl";
+  } else if (80 < props.size && props.size < 112) {
+    return "rounded-2xl";
+  } else {
+    return "rounded-3xl";
+  }
 });
 </script>
 
--- layouts/announcement.vue
+++ layouts/announcement.vue
@@ -1,8 +1,52 @@
 <template>
-  <main class="max-w-[768px] mx-auto p-4 lg:p-0">
-    <div class="m-4">
-      <NavBar :size="64"></NavBar>
+  <div class="relative flex flex-col sm:flex-row">
+    <div class="h-dvh sticky top-0 hidden sm:flex flex-col items-center shadow">
+      <NavBar :size="64" vertical home></NavBar>
     </div>
-    <slot></slot>
-  </main>
+
+    <div
+      class="sticky top-0 z-10 flex sm:hidden p-4 justify-between gap-2 items-center bg-white/50 dark:bg-gray-800/50 bg-opacity-30 backdrop-blur shadow"
+    >
+      <NuxtLink
+        to="/"
+        class="size-[32px] p-0 flex justify-center items-center"
+        variant="outline"
+        color="neutral"
+      >
+        <UIcon class="size-[24px]" name="i-lucide-arrow-left" />
+      </NuxtLink>
+      <div class="flex gap-2 justify-center items-center">
+        <img
+          :src="game?.icon"
+          :alt="game?.name"
+          :title="game?.name"
+          :width="32"
+          :height="32"
+          class="games-icon"
+          :style="{
+            'view-transition-name': `games-icon-${gameKey}`,
+          }"
+        />
+        {{ game?.name }}
+      </div>
+      <div class="size-[32px]"></div>
+    </div>
+
+    <main class="max-w-[768px] mx-auto p-4 lg:p-0">
+      <slot></slot>
+    </main>
+
+    <div class="sm:w-[108px]"></div>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { getGame } from "~/constants/game";
+
+const route = useRoute();
+
+const gameKey = computed(() => {
+  return route.path.replaceAll(/^(\/announcement\/)/g, "");
+});
+const game = computed(() => getGame(gameKey.value));
+</script>
