8f5464731d0676135070633ac8b15668f89099b7
sunfkny <30853461+sunfkny@users.noreply.github.com>
2025-06-07 20:46:01 +0800

    Fix NavBar size

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -4,34 +4,46 @@
       class="flex justify-center items-center gap-2 p-4"
       :class="{ 'flex-col': vertical }"
     >
-      <NuxtLink v-if="home" to="/">
-        <UButton
-          variant="outline"
-          color="neutral"
-          class="p-0"
-          :class="{ [roundedClass]: true }"
+      <NuxtLink
+        v-if="home"
+        to="/"
+        class="border-2 border-solid border-transparent hover:border-gray-300 rounded-2xl"
+      >
+        <div
+          class="p-1 transition-all duration-300 flex justify-center items-center"
+          :style="{
+            width: props.size + 'px',
+            height: props.size + 'px',
+          }"
         >
-          <div class="size-[64px] flex justify-center items-center">
+          <div
+            class="w-full h-full bg-gray-100 rounded-lg flex justify-center items-center"
+          >
             <UIcon name="i-lucide-house" class="size-[32px]" />
           </div>
-        </UButton>
+        </div>
       </NuxtLink>
       <NuxtLink
         v-for="game in games"
         :key="game.key"
-        class="border-2 border-solid border-transparent p-1 transition-all duration-300 hover:border-gray-300"
-        :class="{ [roundedClass]: true }"
         :to="`/announcement/${game.key}`"
+        class="border-2 border-solid border-transparent hover:border-gray-300 rounded-2xl"
       >
-        <img
-          :src="game.icon"
-          :alt="game.name"
-          :title="game.name"
-          :width="props.size"
-          :height="props.size"
-          class="games-icon"
-          :style="{ 'view-transition-name': `games-icon-${game.key}` }"
-        />
+        <div
+          class="p-1 transition-all duration-300"
+          :style="{
+            width: props.size + 'px',
+            height: props.size + 'px',
+          }"
+        >
+          <img
+            :src="game.icon"
+            :alt="game.name"
+            :title="game.name"
+            class="games-icon rounded-lg"
+            :style="{ 'view-transition-name': `games-icon-${game.key}` }"
+          />
+        </div>
       </NuxtLink>
     </div>
   </div>
@@ -52,16 +64,6 @@ const props = withDefaults(
     home: false,
   }
 );
-
-const roundedClass = computed(() => {
-  if (props.size <= 80) {
-    return "rounded-xl";
-  } else if (80 < props.size && props.size < 112) {
-    return "rounded-2xl";
-  } else {
-    return "rounded-3xl";
-  }
-});
 </script>
 
 <style scoped>
