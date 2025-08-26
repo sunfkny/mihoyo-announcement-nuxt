0951d28b435be68cd5c953554f0d58f8399b6f9e
sunfkny <30853461+sunfkny@users.noreply.github.com>
2025-08-26 22:44:30 +0800

    Add shadcn-vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/ui", "@nuxt/eslint"],
+  modules: ["@nuxt/ui", "@nuxt/eslint", "shadcn-nuxt"],
   css: ["~/assets/css/main.css"],
   compatibilityDate: "2025-03-01",
   devtools: { enabled: true },
@@ -17,5 +17,8 @@ export default defineNuxtConfig({
       scan: true,
       icons: ["lucide:x"], // Modal close icon
     },
+  },
+  shadcn: {
+    prefix: "",
   },
 });
